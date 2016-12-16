4ded1487894ae5a339f47f6e6fabf61ba60d11c9
PhungQuocPhu <phungquocphu@gmail.com>
2016-12-16 16:17:52 +0700

    Update InstallJenskinsOnUbuntu.sh

--- InstallJenskinsOnUbuntu.sh
+++ InstallJenskinsOnUbuntu.sh
@@ -2,11 +2,11 @@
 #Company: Soxes VietNam
 #Email: phu@soxes.ch
 #Skype: phu@soxes.ch
-echo '--------------------------------------'
-echo '|========== Install Jenkins ==========|'
-echo '--------------------------------------'
+echo "--------------------------------------"
+echo "|========== Install Jenkins ==========|"
+echo "--------------------------------------"
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+echo 'deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install jenkins
-echo '|---------- Done ----------|'
+echo "|---------- Done ----------|"
